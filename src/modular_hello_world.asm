f3d5503c00f9d97a5568f771301eb24367af151c
Abega1642 <hei.abega.2@gmail.com>
2025-11-24 17:02:47 +0300

    feat: add modular_hello_world.asm

---
+++ src/modular_hello_world.asm
@@ -0,0 +1,22 @@
+
+section .data
+		text db "Modular hello world!", 0x0a
+
+section .text
+		global _start
+
+_start:
+
+	call _print_hello_world
+
+	mov rax, 0x3c
+	mov rdi, 0x00
+	syscall
+
+_print_hello_world:
+	mov rax, 0x01
+	mov rdi, 0x01
+	mov rsi, text
+	mov rdx, 0x15
+	syscall
+	ret
